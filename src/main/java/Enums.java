55d6d60438abc78fb8d4ed0d37bb6cd0fe8d8bae
zPaddyz <jul2009@live.dk>
2023-12-07 13:37:41 +0100

    Opdateret enums

--- src/main/java/Enums.java
+++ src/main/java/Enums.java
@@ -1,16 +1,30 @@
-public class Enums{
-public enum StoreType {
-    LOW,
-    MEDIUM,
-    HIGH,
+public class Enums {
+    public enum StoreType {
+        ALDI,
+        Bilka,
+        LokalBrugsen,
+        DagliBrugsen,
+        SuperBrugsen,
+        TreFemTredsdiscount,
+        Foetex,
+        FoetexFood,
+        Irma,
+        Kvickly,
+        Lidl,
+        LetKoeb,
+        Loevbjerg,
+        MENY,
+        MinKoebmand,
+        Netto,
+        Rema1000,
+        Spar
+    }
 
-}
-public enum ProductType
-{
-    MEAT,
-    VEGAN,
-    BEVERAGE,
-}
+    public enum ProductType {
+        MEAT,
+        VEGAN,
+        BEVERAGE,
+    }
 
 }
 
